395fb5bafce44cee07bd68ae29b05721d3ca66fb
Koldeb <paul.cherruault@gmail.com>
2021-02-15 17:54:31 +0100

    continue tuto

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,25 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
+import Layout from '../../components/layout'
+
+const aze = 'coucou'
 
 export default function FirstPost(){
-    return(<>
-        <h1>Coucou</h1>
+    return(
+    <Layout>
+        <Head>
+            <title>First Post</title>
+        </Head>
+        <Image
+            src='/images/profil.jpg'
+            alt='Photo de Profil'
+            width={250}
+            height={250}
+        />
+        <h1>{aze}</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque qui provident quis? Totam incidunt cum provident? Voluptas blanditiis facere temporibus maxime corporis tenetur quidem, ipsam recusandae. Ex in voluptates ullam.</p>
         <Link href="/"><a>Home</a></Link>
-    </>)
+    </Layout>
+    )
 }
